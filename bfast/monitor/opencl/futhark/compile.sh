a6e5dfcc2c2583a69ecbcab7a1b8675752e56843
mortvest <dmitry.serykh@gmail.com>
2020-11-15 23:53:54 +0100

    GPU code updated to futhark 19.0

--- bfast/monitor/opencl/futhark/compile.sh
+++ bfast/monitor/opencl/futhark/compile.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-FUTHARK_INCREMENTAL_FLATTENING=1 futhark pyopencl --library bfastfinaldetailed.fut
 FUTHARK_INCREMENTAL_FLATTENING=1 futhark pyopencl --library bfastfinal.fut
-futhark pyopencl --library bfastdistrib.fut
-futhark pyopencl --library bfastdistribdetailed.fut
