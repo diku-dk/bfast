29b7ee77f7d608fd1246d0c6109f8628a216947b
12rambau <pierrick.rambaud49@gmail.com>
2021-07-01 14:50:02 +0000

    use apply_along_axis instead of for loop

--- bfast/monitor/python/base.py
+++ bfast/monitor/python/base.py
@@ -150,30 +150,14 @@ class BFASTMonitorPython(BFASTMonitorBase):
             self.magnitudes = rval[:,:,2].astype(np.float32)
             self.valids = rval[:,:,3].astype(np.int32)
         else:
-            means_global = np.zeros((data.shape[1], data.shape[2]), dtype=np.float32)
-            magnitudes_global = np.zeros((data.shape[1], data.shape[2]), dtype=np.float32)
-            breaks_global = np.zeros((data.shape[1], data.shape[2]), dtype=np.int32)
-            valids_global = np.zeros((data.shape[1], data.shape[2]), dtype=np.int32)
-
-            for i in range(data.shape[1]):
-                if self.verbose > 0:
-                    print("Processing row {}".format(i))
-
-                for j in range(data.shape[2]):
-                    y = data[:,i,j]
-                    (pix_break,
-                     pix_mean,
-                     pix_magnitude,
-                     pix_num_valid) = self.fit_single(y)
-                    breaks_global[i,j] = pix_break
-                    means_global[i,j] = pix_mean
-                    magnitudes_global[i,j] = pix_magnitude
-                    valids_global[i,j] = pix_num_valid
-
-            self.breaks = breaks_global
-            self.means = means_global
-            self.magnitudes = magnitudes_global
-            self.valids = valids_global
+            
+            rval = np.apply_along_axis(self.fit_single, 0, data)
+            
+            #print(rval.shape)
+            self.breaks = rval[0].astype(np.int32)
+            self.means = rval[1].astype(np.float32)
+            self.magnitudes = rval[2].astype(np.float32)
+            self.valids = rval[3].astype(np.int32)
 
         return self
 
@@ -227,21 +211,17 @@ class BFASTMonitorPython(BFASTMonitorBase):
         # (1) fit linear regression model for history period
         coef = np.linalg.pinv(X_nn_h@X_nn_h.T)@X_nn_h@y_nn_h
 
-        
-
         if self.verbose > 1:
-            column_names = np.array(["trend",
-                                     "harmonsin1",
+            column_names = np.array(["harmonsin1",
                                      "harmoncos1",
                                      "harmonsin2",
                                      "harmoncos2",
                                      "harmonsin3",
                                      "harmoncos3"])
             if self.trend:
-                indxs = np.array([0, 1, 3, 5, 7, 2, 4, 6])
+                indxs = np.array([1, 3, 5, 7, 2, 4, 6])
             else:
-                indxs = np.array([0, 2, 4, 6, 1, 3, 5])
-            # print(column_names[indxs])
+                indxs = np.array([2, 4, 6, 1, 3, 5])
             print(column_names[indxs])
             print(coef[indxs])
 
@@ -279,11 +259,13 @@ class BFASTMonitorPython(BFASTMonitorBase):
         first_break = np.nonzero(breaks)[0]
 
         if first_break.shape[0] > 0:
-            first_break = first_break[0]
+            first_break = first_break[0].item()
         else:
             first_break = -1
 
-        return first_break, mean, magnitude, Ns
+        rval = np.array([first_break, mean.item(), magnitude.item(), Ns.item()])
+        
+        return rval
 
     def get_timers(self):
         """ Returns runtime measurements for the
